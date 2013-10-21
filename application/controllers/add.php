ace4e05fe83b2c955d56946afca5a634dbbe97d9
nrigaudiere <nicolas.rigaudiere@gmail.com>
2013-10-21 18:01:43 +0200

    Tests

--- application/controllers/add.php
+++ application/controllers/add.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
  
-class Add extends CI_Controller
+class Add extends CI_Controller 
 {
     public function __construct()
     {
